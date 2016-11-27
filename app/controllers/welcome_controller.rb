50049e3edc6efa4d72e52493eb8dcdbe990874c0
soarmarc <soarmarc@soarmarcdeMacBook-Air.local>
2016-11-27 22:50:11 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+	def index
+		flash[:notice] = "早上好。"
+		flash[:alert] = "It is time to go to sleep"
+		flash[:warning] = "It is warning message"
+	end
 end
